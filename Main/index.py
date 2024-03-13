3e9759019b4502f2e59f0782b13d6bc818c3ef19
Alzaabi3974 <alzaabi0004444@gmail.com>
2024-03-14 00:28:47 +0400

    Creted A print function and a test code

--- Main/index.py
+++ Main/index.py
@@ -1,8 +1,15 @@
 """
-Testing to push to github edited by abdull
+Testing to push to github edited by abdulla (edited by abdulla)
 """
 
 class test: 
       
       def __init__(self) -> None:
             pass 
+
+class display:
+
+      def details():
+            x = "Hello"
+            print(x)
+
