11d8f6dfd6f2f34a6da4d13abf5facdf2f6f7a41
rehamad <hamadcif@gmail.com>
2024-03-20 00:03:05 +0400

    S2

--- Main/index.py
+++ Main/index.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+
+
 """
 
 
@@ -5,13 +9,13 @@
 class Task:
       """
       """
-      def __init__(self,title,taskNO,requiredSkills,creditHour,numOfVolunteersNeeded, status) -> None:
-            self.title = title
-            self.taskNo = taskNO
-            self.requiredSkills = requiredSkills
-            self.creditHour = creditHour
-            self.numOfVolunteersNeeded = numOfVolunteersNeeded
-            self.status = status
+      def __init__(self,title = "",taskNO = "",requiredSkills = [],creditHour = 0,numOfVolunteersNeeded = 0, status = "") -> None:
+            self.__title = title
+            self.__taskNo = taskNO
+            self.__requiredSkills = requiredSkills
+            self.__creditHour = creditHour
+            self.__numOfVolunteersNeeded = numOfVolunteersNeeded
+            self.__status = status
 
             
 
@@ -20,35 +24,76 @@ class Task:
 class Volunteer_Opportunity:
       """
       """
-      def __init__(self) -> None:
-            pass
+      def __init__(self,title,data,startingTime,endingTime,location) -> None:
+            self.__title = title
+            self.__data = data
+            self.__startingTime = startingTime
+            self.__endingTime = endingTime
+            self.__location = location
+            
+            
       
       
       
 class Organization:
       """
       """
-      def __init__(self) -> None:
-            pass
+      def __init__(self,organization_name,description,organizationID) -> None:
+            self.__organization_name = organization_name
+            self.__description = description
+            self.__organizationID = organizationID
+            
+            
       
 
 class User:
       """
       Super class of three other classes (Administrator, Organization Representative, and Volunteer)
       """
-      def __init__(self) -> None:
-            pass
+      userRecord = []
+      
+      today = datetime.now()
+      year = str(today.year)
+      month = str(today.month)
+      nums = str("000")
+      strOfID = str(year + "0" + month + nums)
+      randomID = int(strOfID)
+      
+      def __init__(self,userID, fullName, mobile,email,educationLevel, DOJ,DOB) -> None:
+            User.randomID += 1
+            self.__userID = User.randomID
+            self.__fullName = fullName
+            self.__mobile = mobile
+            self.__email = email
+            self.__educationLevel = educationLevel
+            self.__DOJ = DOJ
+            self.__DOB = DOB
+            
+
+
  
  
 class Volunteer(User):
       """
-      """     
-      def __init__(self) -> None:
+      """
+      volunteerRecord = []     
+      
+      
+      def __init__(self) -> None: 
             pass # add super().__init__ after fixing user attributes
 class Organization_Representative(User):
       """
       """
+      organizationRepRecord = []
+
+      
       def __init__(self) -> None:
             pass # add super().__init__ after fixing user attributes
       
+class Administrator(User):
+      """
+      """
+      administratorRecord = []
       
+      def __init__(self) -> None:
+            pass # add super().__init__ after fixing user attributes      
