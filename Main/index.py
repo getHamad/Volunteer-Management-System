ff36cc7516f960e18d9ad9667fca38e141b1e19b
rehamad <hamadcif@gmail.com>
2024-03-20 02:17:41 +0400

    S3

--- Main/index.py
+++ Main/index.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-
+from datetime import datetime, date
 
 
 """
@@ -9,14 +8,18 @@ from datetime import datetime
 class Task:
       """
       """
-      def __init__(self,title = "",taskNO = "",requiredSkills = [],creditHour = 0,numOfVolunteersNeeded = 0, status = "") -> None:
-            self.__title = title
-            self.__taskNo = taskNO
-            self.__requiredSkills = requiredSkills
-            self.__creditHour = creditHour
-            self.__numOfVolunteersNeeded = numOfVolunteersNeeded
-            self.__status = status
 
+      taskRecord = {}      
+      def __init__(self) -> None:
+            self.__title = str(input("Task title: "))
+            self.__taskNo = str(input("Task number: "))
+            self.__requiredSkills = str(input("skills separated by comma: ").split(","))
+            self.__creditHour = int(input("Credit Hours: "))
+            self.__numOfVolunteersNeeded = int(input("Number of Volunteers: "))
+            self.__status = str(input("Status"))
+            Task.taskRecord [self.__taskNo] = {
+                  
+            }
             
 
 
@@ -24,9 +27,14 @@ class Task:
 class Volunteer_Opportunity:
       """
       """
-      def __init__(self,title,data,startingTime,endingTime,location) -> None:
+      opportunityDict = {}
+      opportunityCounter = 0
+      def __init__(self,
+                   title = '', date = date, startingTime = '', endingTime = '', location = ''
+                   ) -> None:
+            Volunteer_Opportunity.opportunityCounter += 1
             self.__title = title
-            self.__data = data
+            self.__date = date
             self.__startingTime = startingTime
             self.__endingTime = endingTime
             self.__location = location
@@ -38,7 +46,9 @@ class Volunteer_Opportunity:
 class Organization:
       """
       """
-      def __init__(self,organization_name,description,organizationID) -> None:
+      def __init__(self,
+                   organization_name = '', description = '', organizationID = ''
+                   ) -> None:
             self.__organization_name = organization_name
             self.__description = description
             self.__organizationID = organizationID
@@ -50,7 +60,7 @@ class User:
       """
       Super class of three other classes (Administrator, Organization Representative, and Volunteer)
       """
-      userRecord = []
+      userRecord = {}
       
       today = datetime.now()
       year = str(today.year)
@@ -59,15 +69,18 @@ class User:
       strOfID = str(year + "0" + month + nums)
       randomID = int(strOfID)
       
-      def __init__(self,userID, fullName, mobile,email,educationLevel, DOJ,DOB) -> None:
+      def __init__(self, 
+                   ) -> None:
             User.randomID += 1
             self.__userID = User.randomID
-            self.__fullName = fullName
-            self.__mobile = mobile
-            self.__email = email
-            self.__educationLevel = educationLevel
-            self.__DOJ = DOJ
-            self.__DOB = DOB
+            print("Enter the following information")
+            self.__fullName = str(input("full name: "))
+            self.__mobile = str(input("mobile number: "))
+            self.__email = str(input("email: "))
+            self.__educationLevel = str(input("education level: "))
+            self.__DOB = input("date of birth (yyyy-mm-dd): ").split("-")
+            self.__DOB = date(int(self.__DOJ[0]), int(self.__DOJ[1]), int(self.__DOJ[2]))
+            self.__DOJ = datetime.today()
             
 
 
@@ -76,24 +89,62 @@ class User:
 class Volunteer(User):
       """
       """
-      volunteerRecord = []     
-      
-      
-      def __init__(self) -> None: 
-            pass # add super().__init__ after fixing user attributes
+      volunteerRecord = {}
+
+      def __init__(self,
+                   ) -> None:
+            super().__init__()
+            self.__userID = "V" + str(self._User__userID)
+            self.__skills = input("skills separated by comma: ").split(",")
+            self.__tasks = []
+            self.__completedTasks = []
+            self.__totalVolunteeringHrs = 0
+            
+            
+            Volunteer.volunteerRecord [self.__userID] = {
+                  "skills": self.__skills,
+                  "tasks": self.__tasks,
+                  "completedTasks": self.__completedTasks,
+               
+            }
+            
+            
+            User.userRecord [self.__userID] = {
+                  "fullname": self._User__fullName,
+                  "mobile": self._User__mobile,
+                  "email": self._User__email,
+                  "educationLevel": self._User__educationLevel,
+                  "DOJ": self._User__DOJ,
+                  "DOB": self._User__DOB      
+            }
+            
+            
+      def test(self): 
+            return self.__userID
 class Organization_Representative(User):
       """
       """
-      organizationRepRecord = []
+      organizationRepRecord = {}
 
       
-      def __init__(self) -> None:
-            pass # add super().__init__ after fixing user attributes
-      
+      def __init__(self, 
+                   fullName, mobile, email, educationLevel, DOJ, DOB
+                   ) -> None:
+            super().__init__( fullName, mobile, email, educationLevel, DOJ, DOB)
+            self.__userID = "O" + str(self._User__userID)
+
+            
 class Administrator(User):
       """
       """
       administratorRecord = []
       
-      def __init__(self) -> None:
-            pass # add super().__init__ after fixing user attributes      
+      def __init__(self, 
+                   userID, fullName, mobile, email, educationLevel, DOJ, DOB
+                   )-> None:
+            super().__init__(userID, fullName, mobile, email, educationLevel, DOJ, DOB)
+            self.__userID = "A" + str(self._User__userID)
+
+
+
+
--- Main/test.py
+++ Main/test.py
@@ -1,3 +1,51 @@
 from index import *
-display.details()
 
+
+
+d = {}
+def test( id, name = ""): 
+      id = id
+      name = name
+      d [id] = {
+            "name": name
+      }
+      
+      return "done"
+      
+test(1, "hamad a almazrouei")
+test(2, "ahmed")
+test(3, "rashid")
+print(d)
+
+
+
+"""for i in d.items(): 
+      print(i) """
+      
+def update_id(id_to_update, new_id):
+    if id_to_update in d:
+        d[new_id] = d.pop(id_to_update)  # Update id and preserve associated data
+        print(f"Updated id {id_to_update} to {new_id}")
+    else:
+        print(f"Id {id_to_update} not found in the dictionary")
+
+# Example: Update id 2 to 4
+update_id(2, 4)
+print(d)     
+
+
+
+for i, data, in d.items():
+      if i == 1:
+            print(data['name'])
+      else: 
+            pass
+            
+            
+print("\n\n")
+v1 = Volunteer()
+v2 = Volunteer()
+
+print(Volunteer.volunteerRecord)
+print("\ngdfgdgdfg\n")
+print(f"{User.userRecord}", )
